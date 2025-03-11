b3c601767f38be99258e603d27c6c70025546ff2
Karthik Shetty <54941058+karthikshetty26@users.noreply.github.com>
2025-03-12 00:25:31 +0530

    Google analytics script added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import NavbarUi from '@/components/navbar_ui/navbar_ui'
+import Script from "next/script";
 
 export const metadata = {
   metadataBase: new URL('https://karthikshetty.info'),
@@ -45,8 +46,16 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
-
         <link href="https://fonts.googleapis.com/css2?family=Jersey+10&display=swap" rel="stylesheet" />
+
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-9J1RVTSWHQ" />
+        <Script id='google-analytics' strategy='afterInteractive'>{`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-9J1RVTSWHQ');
+        `}</Script>
       </head>
       <body>
         <NavbarUi />
