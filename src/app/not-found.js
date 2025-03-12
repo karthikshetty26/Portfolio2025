4a31781049e5fb06504095d77960e4df585c9e07
Karthik Shetty <54941058+karthikshetty26@users.noreply.github.com>
2025-03-12 16:48:23 +0530

    404 page updated

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,6 +1,12 @@
+import ERROR from './Error.module.css';
+
 export default function NotFound() {
     return (<>
-        <h1>404</h1>
+        <main className={ERROR.main}>
+            <h1 className={ERROR.error_h1}>404</h1>
+            <p className={ERROR.error_p}>Don't go here and there! Let's start from the beginning.</p>
+            <a className={ERROR.error_a} href="/">Back to Home</a>
+        </main>
     </>
     )
 }
