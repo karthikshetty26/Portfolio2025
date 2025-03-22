5c68f4d5f32a5091559b43b2a8b923da18da378c
Karthik Shetty <54941058+karthikshetty26@users.noreply.github.com>
2025-03-22 18:07:56 +0530

    Navbar updated (Dark & light mood)

--- src/components/navbar_ui/navbar_ui.js
+++ src/components/navbar_ui/navbar_ui.js
@@ -32,7 +32,7 @@ export default function NavbarUi() {
     // Effect to update showMenu when pathname changes
     useEffect(() => {
         setShowMenu(pathname === '/');
-        
+
         // If navigating away from root, close the menu if it's open
         if (pathname !== '/' && isMenuOpen) {
             setIsMenuOpen(false);
@@ -87,8 +87,16 @@ export default function NavbarUi() {
             {/* Theme toggle */}
             <div className={`${navCSS.theme_div} ${!showMenu ? navCSS.only_theme : ''}`} onClick={toggleTheme}>
                 {theme === 'light' ? 'Dark' : 'Light'}&nbsp;&nbsp;
+
                 <button className={navCSS.dark_light_btn}>
-                    <span className={theme === 'light' ? navCSS.light : navCSS.dark}></span>
+                    <span>
+                        {theme === 'light' ? (
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"></path></svg>
+                        ) : (
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M11.3807 2.01886C9.91573 3.38768 9 5.3369 9 7.49999C9 11.6421 12.3579 15 16.5 15C18.6631 15 20.6123 14.0843 21.9811 12.6193C21.6613 17.8537 17.3149 22 12 22C6.47715 22 2 17.5228 2 12C2 6.68514 6.14629 2.33869 11.3807 2.01886Z"></path></svg>
+                        )}
+
+                    </span>
                 </button>
             </div>
 
