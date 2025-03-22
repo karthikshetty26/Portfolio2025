ef30ad707576f09f12e85292cb932011e1696deb
Karthik Shetty <54941058+karthikshetty26@users.noreply.github.com>
2025-03-22 18:08:21 +0530

    Project info updated

--- src/app/projects/[project]/page.js
+++ src/app/projects/[project]/page.js
@@ -29,12 +29,12 @@ export default async function Page({ params }) {
 
                 {/* Project logo and name container */}
                 {postData.title && (
-                    <span>
+                    <span className="mb_fix">
                         {/* Conditional rendering for project logo */}
                         {postData.logo && (
                             <Image
                                 src={postData.logo}
-                                alt={'Webtoolsmith Logo'}
+                                alt={postData.title}
                                 width={80}
                                 height={80}
                             />
@@ -45,15 +45,74 @@ export default async function Page({ params }) {
                     </span>
                 )}
 
+                {/* Project description */}
+                {postData.description && (
+                    <div className="about large project_dec mb_fix">
+                        <h5>About the project</h5>
+                        <p>{postData.description}</p>
+                    </div>
+                )}
+
+                {/* My role & challenges in the project */}
+                {postData.myRole && (
+                    <div className="my_role large project_dec mb_fix">
+                        <h5>My Role & Challenges</h5>
+                        <p>{postData.myRole}</p>
+                    </div>
+                )}
+
+                <div className="tiny_info_container mb_fix">
+                    {/* Project year information */}
+                    {postData.year && (
+                        <div className="date project_dec">
+                            <h5>Year</h5>
+                            <p>{postData.year}</p>
+                        </div>
+                    )}
+
+                    {/* Project category information */}
+                    {postData.category && (
+                        <div className="category project_dec">
+                            <h5>Category</h5>
+                            <p>{postData.category}</p>
+                        </div>
+                    )}
+
+                    {/* Project platform information */}
+                    {postData.platform && (
+                        <div className="platform project_dec">
+                            <h5>Platform</h5>
+                            <p>{postData.platform}</p>
+                        </div>
+                    )}
+
+                    {/* Project type information */}
+                    {postData.type && (
+                        <div className="date project_dec">
+                            <h5>Project Type</h5>
+                            <p>{postData.type}</p>
+                        </div>
+                    )}
+                </div>
+
+                {/* Technologies used in the project (conditional rendering) */}
+                {postData.tech && (
+                    <div className="tech project_dec mb_fix">
+                        <h5>Tech & Technique</h5>
+                        <p>{postData.tech}</p>
+                    </div>
+                )}
+
                 {/* Actions section with external links */}
                 {postData.projectLinks && (
-                    <div className="acctions project_dec">
-                        <h5>Actions</h5>
+                    <div className="acctions btns project_dec mb_fix">
                         <ul>
                             {postData.projectLinks.map((link, index) => (
-                                <li key={index} className="hover">
+                                <li key={index}>
                                     <a href={link.url} target="_blank" rel="noopener noreferrer">
                                         {link.name}
+
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M1.99974 13.0001L1.9996 11.0002L18.1715 11.0002L14.2218 7.05044L15.636 5.63623L22 12.0002L15.636 18.3642L14.2218 16.9499L18.1716 13.0002L1.99974 13.0001Z"></path></svg>
                                     </a>
                                 </li>
                             ))}
@@ -61,37 +120,6 @@ export default async function Page({ params }) {
                     </div>
                 )}
 
-                {/* Project year information */}
-                {postData.year && (
-                    <div className="date project_dec">
-                        <h5>Year</h5>
-                        <p>{postData.year}</p>
-                    </div>
-                )}
-
-                {/* Technologies used in the project (conditional rendering) */}
-                {postData.tech && (
-                    <div className="tech project_dec">
-                        <h5>Tech & Technique</h5>
-                        <p>{postData.tech}</p>
-                    </div>
-                )}
-
-                {/* Project description */}
-                {postData.description && (
-                    <div className="tech project_dec">
-                        <h5>Description</h5>
-                        <p>{postData.description}</p>
-                    </div>
-                )}
-
-                {/* My role in the project */}
-                {postData.myRole && (
-                    <div className="my_role project_dec">
-                        <h5>My Role</h5>
-                        <p>{postData.myRole}</p>
-                    </div>
-                )}
             </div>
 
             {/* Project images gallery with lazy loading */}
@@ -103,15 +131,30 @@ export default async function Page({ params }) {
                             <Image
                                 src={image.src} // Correct reference to `image.src`
                                 alt={image.alt}
-                                width={600}
-                                height={400}
+                                width={1366}
+                                height={768}
                                 priority={index === 0} // Prioritize first image
                                 loading={index === 0 ? "eager" : "lazy"} // Lazy load other images
-                                sizes="(max-width: 768px) 100vw, 95vw"
+                                // sizes="(max-width: 768px) 100vw, 95vw"
                                 quality={75} // Balanced quality for performance
                             />
                         </div>
                     ))}
+                </div>
+            )}
+
+            {/* CAT */}
+            {postData.cat && (
+                <div className="acctions project_dec mb_fix">
+                    <ul>
+                        {postData.cat.map((link, index) => (
+                            <li key={index} className="hover">
+                                <a href={link.url} target="_blank" rel="noopener noreferrer">
+                                    {link.name}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             )}
         </main>
