0ea3b6f402492d150d5a236d6feb9305667f27f5
Karthik Shetty <54941058+karthikshetty26@users.noreply.github.com>
2025-03-29 16:24:40 +0530

    Application icons 'metadata' updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -35,6 +35,27 @@ export const metadata = {
     index: true,
     follow: true
   },
+  icons: {
+    icon: [
+      {
+        url: '/icons/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'image/png'
+      },
+      {
+        url: '/icons/favicon-16x16.png',
+        sizes: '16x16',
+        type: 'image/png'
+      }
+    ],
+    apple: [
+      {
+        url: '/icons/apple-touch-icon.png',
+        sizes: '180x180'
+      }
+    ],
+    shortcut: '/icons/favicon-16x16.png'
+  },
   alternates: {
     canonical: "/"
   }
